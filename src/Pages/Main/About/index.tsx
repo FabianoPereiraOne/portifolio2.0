bbba81e11e6eb50ee959aaa8fc823dd92797c1cf
Fabiano Pereira <fabianop971@gmail.com>
2021-11-14 21:23:54 -0300

    Feat ( About ): Add and formated text

--- src/Pages/Main/About/index.tsx
+++ src/Pages/Main/About/index.tsx
@@ -7,9 +7,12 @@ function About() {
         <main className={Styled.content_about} id="sobre">
             <Title title="Sobre mim" subTitle="Um breve resumo." />
             <article className={Styled.about}>
-                <p>Sou Fabiano Pereira, trabalho como Developer FrontEnd meu diferencial é que sou esforçado e atento ao que me foi proposto, tenho experiencia na criação de Landing Pages, Dashboards, Websites e Sistemas intermediários, bastante solicitados por afiliados de hotmart, eduzz e monetizze. Meu principal foco é em melhorar a entrega dos Websites e Sistemas com a inclusão do modelo clean code, melhor segurança e um site mais adaptável aos diversos dispositivos existentes.
+                <p>Sou Fabiano Pereira, trabalho como Developer FrontEnd, meu diferencial é que sou esforçado e atento ao que me foi proposto, tenho experiencia na criação de Landing Pages, Dashboards, Websites e Sistemas intermediários, bastante solicitados por afiliados de hotmart, eduzz e monetizze. Meu principal foco é em melhorar a entrega dos Websites e Sistemas deixando-os mais modernos, seguros e responsivos.
+                </p>
 
-                <cite>A arte de programar consiste na arte de organizar e dominar a complexidade".</cite><i>Dijkstra</i></p>
+                <p>
+                    <cite>"A arte de programar consiste na arte de organizar e dominar a complexidade."</cite><small>Dijkstra</small>
+                </p>
                 <ButtonPrimary>
                     Download Cv
                 </ButtonPrimary>
