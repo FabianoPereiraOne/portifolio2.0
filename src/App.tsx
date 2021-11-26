1c43e9303d9ce335805b65180fb7508cb47a6c55
Fabiano Pereira <fabianop971@gmail.com>
2021-11-25 21:34:45 -0300

    Feat ( Toast ): Limit popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import Routes from './Routes'
 import { PortfolioProvider } from './contexts'
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
-      <PortfolioProvider>
-            <ToastContainer autoClose={ 3000 }/>
-        <Routes/>
-      </PortfolioProvider>
-  );
+    <PortfolioProvider>
+      <ToastContainer autoClose={3000} limit={1} />
+      <Routes />
+    </PortfolioProvider>
+  )
 }
 
-export default App;
+export default App
