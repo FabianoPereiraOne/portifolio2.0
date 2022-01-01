1146ca1e3dcfbd677d28f8c01372f877bb990161
Fabiano Pereira <fabianop971@gmail.com>
2022-01-01 19:43:08 -0300

    Feat ( Home ): Finish animation code

--- src/Pages/Main/Home/index.tsx
+++ src/Pages/Main/Home/index.tsx
@@ -1,23 +1,89 @@
 import * as Scroll from 'react-scroll'
-import Styled from './home.module.css'
+import styles from './home.module.css'
 import logo from '../../../assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { FiMenu } from 'react-icons/fi'
 import { ProgressBar, ButtonPrimary } from '../../../Components/Global'
-import { useState } from 'react'
+import { useRef, useState, useEffect, MutableRefObject } from 'react'
 
 function Inicio() {
   const [progress, setProgress] = useState(0)
   const [toggleMenu, setToggleMenu] = useState(false)
+  const textPrinter: MutableRefObject<null | HTMLSpanElement> = useRef(null)
+  const cursor: MutableRefObject<null | HTMLSpanElement> = useRef(null)
+  const AllTextPrinter = ['Front-End', 'Web', 'Moderno']
+  const delayAnimation = 2
+  const delayToggleText = 4
+  const speedWord = 13
+  let charIndex = 0
+  let textIndex = 0
+
+  const handleGetWord = () => {
+    if (textIndex < AllTextPrinter.length) {
+      handleGetCharText(AllTextPrinter[textIndex])
+      setTimeout(() => {
+        textIndex++
+        handleGetWord()
+      }, delayToggleText * 1000)
+    } else {
+      textIndex = 0
+      charIndex = 0
+      handleGetWord()
+    }
+  }
+
+  const handleGetCharText = (text: string) => {
+    const char = text.charAt(charIndex)
+    if (charIndex < text.length) {
+      setTimeout(() => {
+        handlePrinterCharText(char)
+        charIndex++
+        handleGetCharText(text)
+      }, speedWord * 10)
+    } else {
+      setTimeout(() => handleClearText(text), 1000)
+    }
+  }
+
+  const handlePrinterCharText = (char: string) => {
+    if (textPrinter) {
+      textPrinter.current!.textContent += char
+    }
+  }
+
+  const handlePrinterClearCharText = (char: string) => {
+    if (textPrinter) {
+      textPrinter.current!.textContent = char
+    }
+  }
+
+  const handleClearText = (text: string) => {
+    if (charIndex > 0) {
+      const word = AllTextPrinter[textIndex].substring(0, charIndex - 1)
+      setTimeout(() => {
+        handlePrinterClearCharText(word)
+        charIndex--
+        handleClearText(text)
+      }, speedWord * 10)
+    }
+  }
+
+  useEffect(() => {
+    if (AllTextPrinter.length > 0 && textPrinter && cursor) {
+      setTimeout(handleGetWord, delayAnimation * 1000)
+    }
+
+    //eslint-disable-next-line
+  }, [])
 
   function handleScroll(progress: number) {
     setProgress(progress)
   }
 
   return (
-    <section className={Styled.section_home} id="inicio">
+    <section className={styles.section_home} id="inicio">
       {toggleMenu && (
-        <ul className={Styled.menu_mobile}>
+        <ul className={styles.menu_mobile}>
           <li>
             <Scroll.Link
               onClick={() => setToggleMenu(false)}
@@ -57,12 +123,12 @@ function Inicio() {
         </ul>
       )}
 
-      <header className={Styled.header}>
-        <Link to="/" className={Styled.container_logo}>
+      <header className={styles.header}>
+        <Link to="/" className={styles.container_logo}>
           <img src={logo} alt="logo" />
           <p>Fabiano Pereira</p>
         </Link>
-        <nav className={Styled.nav}>
+        <nav className={styles.nav}>
           <Scroll.Link
             onMouseEnter={() => handleScroll(0)}
             to="inicio"
@@ -91,26 +157,30 @@ function Inicio() {
           >
             Projetos
           </Scroll.Link>
-          <span className={Styled.progress_bar}>
+          <span className={styles.progress_bar}>
             <ProgressBar progressValue={progress} />
           </span>
         </nav>
 
         <button
-          className={Styled.btn_menu_mobile}
+          className={styles.btn_menu_mobile}
           onClick={() => setToggleMenu(!toggleMenu)}
         >
           <FiMenu />
         </button>
       </header>
-      <section className={Styled.section_banner_default}>
+      <section className={styles.section_banner_default}>
         <h1>Fabiano Pereira</h1>
-        <div className={Styled.content_text_code}>
-          <span className={Styled.key_green}>{'{'}</span>
-          <p>Desenvolvedor Front-End</p>
-          <span className={Styled.key_green}>{'}'}</span>
+        <div className={styles.content_text_code}>
+          <p>
+            Desenvolvedor{' '}
+            <span ref={textPrinter} className={styles.text_printer}></span>
+            <span ref={cursor} className={styles.bar_cursor}>
+              &nbsp;
+            </span>
+          </p>
         </div>
-        <div className={Styled.btn_group}>
+        <div className={styles.btn_group}>
           <Scroll.Link to="sobre" smooth>
             Sobre
           </Scroll.Link>
